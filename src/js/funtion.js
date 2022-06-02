8b21cc1d0b6186310ef34514535ee769bb606cbc
XiaoGoanYu <anhsonpv@gmail.com>
2022-06-02 09:05:37 +0700

    thay doi

---
+++ src/js/funtion.js
@@ -0,0 +1,39 @@
+const a = 10;
+const b = 20;
+
+function sum(a, b) {
+    // check
+    if (typeof a !== "number" || typeof b !== "number") return -1;
+    // processing
+    const total = a + b;
+    // return
+    return total;
+}
+sum(a, b);
+
+const products = [
+    { id: 1, name: "Product A" }, // 1 -> index: 0 -
+    { id: 2, name: "Product B" }, // 2 -> index: 1
+];
+
+function listProduct(products) {
+    // check
+    if (!Array.isArray(products) || products.length == 0) return false;
+    // processing
+
+    let result = "";
+    for (let i = 0; i < products.length; i++) {
+        result += `<h2>${products[i].name}</h2>`;
+    }
+    // return
+    return result;
+}
+
+function render(elementId, content) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.innerHTML = content;
+    }
+}
+render("total", sum(a, b));
+render("products", listProduct(products));
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
-import './style.css'
+const products = [
+    { id: 1, name: "Product A", img: "https://picsum.photos/400/400" },
+    { id: 2, name: "Product B", img: "https://picsum.photos/400/400" },
+];
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function listProduct(products) {
+    if (!Array.isArray(products) || products.length == 0) return false;
+
+    let result = "";
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        result += `
+              <div>
+                  <img src="${product.img}" alt="" />
+                  <h3><a href="">${product.name}</a></h3>
+              </div>
+      `;
+    }
+    return result;
+}
+
+function render(elementId, content) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.innerHTML = content;
+    }
+}
+render("products", listProduct(products));
