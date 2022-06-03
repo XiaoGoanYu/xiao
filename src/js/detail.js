5ffd517895e6cb2e3f669451328206d788981716
XiaoGoanYu <anhsonpv@gmail.com>
2022-06-03 10:41:25 +0700

    thayd oi

---
+++ src/js/detail.js
@@ -0,0 +1,14 @@
+// Lấy ID trên url
+const id = new URLSearchParams(window.location.search).get("id");
+
+// tìm kiếm trong mảng products
+// nếu ID phần tử trong mảng == id trên url
+// thì trả về kết quả là cái phần tử tìm được
+const products = [
+    { id: 1, name: SonA, img: "https://picsum.photos/400/400" },
+    { id: 2, name: NamA, img: "https://picsum.photos/400/400" },
+];
+
+const found =products.find(function(product) {
+    return 
+})
